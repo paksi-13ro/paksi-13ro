9d131f4d30ea6f5a1e00ba979cb1ea76d0036485
paksi-13ro <a9145795026@gmail.com>
2025-01-05 13:48:19 +0300

    Add files via upload

--- rar.py
+++ rar.py
@@ -1,74 +1,112 @@
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import PhotoImage, messagebox
+from PIL import Image, ImageTk
 import pygame
+import pythoncom
 import win32com.client
 
 
-class MediaPlayer:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Media Player")
-        self.master.geometry("400x200")
+class MediaViewer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Media Viewer")
 
-        self.playlist = []
+        # Инициализация Pygame
+        pygame.init()
+
+        # Переменные
+        self.media_files = []
         self.current_index = 0
 
-        self.label = tk.Label(master, text="Media Player", font=("Arial", 16))
+        # UI компоненты
+        self.label = tk.Label(root)
         self.label.pack()
 
-        self.play_button = tk.Button(master, text="Play", command=self.play)
+        self.play_button = tk.Button(root, text="Play", command=self.play_media)
         self.play_button.pack(side=tk.LEFT)
 
-        self.pause_button = tk.Button(master, text="Pause", command=self.pause)
+        self.pause_button = tk.Button(root, text="Pause", command=self.pause_media)
         self.pause_button.pack(side=tk.LEFT)
 
-        self.stop_button = tk.Button(master, text="Stop", command=self.stop)
+        self.stop_button = tk.Button(root, text="Stop", command=self.stop_media)
         self.stop_button.pack(side=tk.LEFT)
 
-        self.next_button = tk.Button(master, text="Next", command=self.next_media)
-        self.next_button.pack(side=tk.LEFT)
-
-        self.prev_button = tk.Button(master, text="Previous", command=self.prev_media)
+        self.prev_button = tk.Button(root, text="Previous", command=self.prev_media)
         self.prev_button.pack(side=tk.LEFT)
 
-        self.load_button = tk.Button(master, text="Load Shortcuts", command=self.load_shortcuts)
-        self.load_button.pack(side=tk.LEFT)
+        self.next_button = tk.Button(root, text="Next", command=self.next_media)
+        self.next_button.pack(side=tk.LEFT)
 
-        pygame.mixer.init()
+    def load_shortcuts(self, shortcut_paths):
+        self.media_files = [self.resolve_shortcut(path) for path in shortcut_paths]
+        self.show_media()
 
-    def load_shortcuts(self):
-        shortcuts = filedialog.askopenfilenames(title="Select Shortcut Files", filetypes=[("Shortcut Files", "*.lnk")])
-        self.playlist = [self.resolve_shortcut(lnk) for lnk in shortcuts]
-        self.current_index = 0
-        print("Loaded:", self.playlist)
-
-    def resolve_shortcut(self, lnk_path):
+    def resolve_shortcut(self, shortcut_path):
         shell = win32com.client.Dispatch("WScript.Shell")
-        shortcut = shell.CreateShortCut(lnk_path)
-        return shortcut.Targetpath
+        shortcut = shell.CreateShortCut(shortcut_path)
+        return shortcut.TargetPath
 
-    def play(self):
-        if self.playlist:
-            pygame.mixer.music.load(self.playlist[self.current_index])
-            pygame.mixer.music.play()
+    def show_media(self):
+        if not self.media_files:
+            messagebox.showerror("Error", "No media files loaded.")
+            return
 
-    def pause(self):
+        media_file = self.media_files[self.current_index]
+        ext = os.path.splitext(media_file)[1].lower()
+
+        if ext in [".jpg", ".jpeg", ".png", ".gif"]:
+            self.show_image(media_file)
+        elif ext in [".mp4", ".avi", ".mov"]:
+            self.show_video(media_file)
+        elif ext in [".mp3", ".wav"]:
+            self.show_audio(media_file)
+
+    def show_image(self, filepath):
+        img = Image.open(filepath)
+        img = img.resize((800, 600), Image.ANTIALIAS)
+        img_tk = ImageTk.PhotoImage(img)
+        self.label.config(image=img_tk)
+        self.label.image = img_tk
+
+    def show_video(self, filepath):
+        self.stop_media()  # Остановить предыдущее воспроизведение
+        pygame.display.set_mode((800, 600))
+        pygame.mixer.music.load(filepath)
+        self.label.config(text="Playing video...")
+        pygame.mixer.music.play()
+
+    def show_audio(self, filepath):
+        self.stop_media()  # Остановить предыдущее воспроизведение
+        pygame.mixer.music.load(filepath)
+        self.label.config(text="Playing audio...")
+        pygame.mixer.music.play()
+
+    def play_media(self):
+        pygame.mixer.music.unpause()
+
+    def pause_media(self):
         pygame.mixer.music.pause()
 
-    def stop(self):
+    def stop_media(self):
         pygame.mixer.music.stop()
-
-    def next_media(self):
-        self.current_index = (self.current_index + 1) % len(self.playlist)
-        self.play()
+        self.label.config(text="")
 
     def prev_media(self):
-        self.current_index = (self.current_index - 1) % len(self.playlist)
-        self.play()
+        self.current_index = (self.current_index - 1) % len(self.media_files)
+        self.show_media()
+
+    def next_media(self):
+        self.current_index = (self.current_index + 1) % len(self.media_files)
+        self.show_media()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    media_player = MediaPlayer(root)
+    viewer = MediaViewer(root)
+
+    # Пример загрузки ярлыков
+    shortcuts = ["C:\\path\\to\\your\\shortcut1.lnk", "C:\\path\\to\\your\\shortcut2.lnk"]
+    viewer.load_shortcuts(shortcuts)
+
     root.mainloop()
